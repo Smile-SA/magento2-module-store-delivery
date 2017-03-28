c543d376d0e25c3d9aa8b25f36e59eb9834ed21b
Romain Ruaud <romain.ruaud@smile.fr>
2017-03-28 16:51:51 +0200

    Added Carrier.

---
+++ Model/Carrier.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\StorePickup
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2017 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\StorePickup\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\DataObject;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\ResultFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Store Pickup Carrier model
+ * @SuppressWarnings(PHPMD.CamelCasePropertyName)
+ *
+ * @category Smile
+ * @package  Smile\StorePickup
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class Carrier extends AbstractCarrier implements CarrierInterface
+{
+    /**
+     * @var string
+     */
+    protected $_code = 'smile_store_pickup';
+
+    /**
+     * Whether this carrier has fixed rates calculation
+     *
+     * @var boolean
+     */
+    protected $_isFixed = true;
+
+    /**
+     * @var ResultFactory
+     */
+    protected $rateResultFactory;
+
+    /**
+     * @var MethodFactory
+     */
+    protected $rateMethodFactory;
+
+    /**
+     * Carrier constructor
+     *
+     * @param ScopeConfigInterface $scopeConfig       Scope Configuration
+     * @param ErrorFactory         $rateErrorFactory  Rate error Factory
+     * @param LoggerInterface      $logger            Logger
+     * @param ResultFactory        $rateResultFactory Rate result Factory
+     * @param MethodFactory        $rateMethodFactory Rate method Factory
+     * @param array                $data              Carrier Data
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        ErrorFactory $rateErrorFactory,
+        LoggerInterface $logger,
+        ResultFactory $rateResultFactory,
+        MethodFactory $rateMethodFactory,
+        array $data = []
+    ) {
+        $this->rateResultFactory = $rateResultFactory;
+        $this->rateMethodFactory = $rateMethodFactory;
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAllowedMethods()
+    {
+        return [$this->getCarrierCode() => __($this->getConfigData('name'))];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        /** @var \Magento\Shipping\Model\Rate\Result $result */
+        $result = $this->rateResultFactory->create();
+
+        /** @var \Magento\Quote\Model\Quote\Address\RateResult\Method $method */
+        $method = $this->rateMethodFactory->create();
+
+        $method->setCarrier($this->getCarrierCode());
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod($this->getCarrierCode());
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        $amount = $this->getConfigData('price');
+
+        $method->setPrice($amount);
+        $method->setCost($amount);
+
+        $result->append($method);
+
+        return $result;
+    }
+}
